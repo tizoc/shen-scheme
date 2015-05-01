6a72e91d0f23a85ca71bf6c58580c65471c3b461
Bruno Deferrari <utizoc@gmail.com>
2015-04-30 22:50:34 -0300

    Add 'append' mode to `open`

--- shen/primitives.scm
+++ shen/primitives.scm
@@ -223,6 +223,9 @@
                 (open-input-file full-path)
                 (error "File does not exist" full-path)))
       ((out) (open-output-file full-path))
+      ((append) (let ((fd (open full-path
+                                (+ open/create open/write open/append))))
+                  (open-output-file-descriptor fd)))
       (else (error "Invalid direction" direction)))))
 
 (define (kl:close stream)
