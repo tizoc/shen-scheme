3523205fe5ad4259ab1d59841ae269f1552c76cb
Bruno Deferrari <utizoc@gmail.com>
2019-09-22 17:06:54 -0300

    Bump version.

--- src/init.scm
+++ src/init.scm
@@ -3,7 +3,7 @@
 
 (kl:set (quote *language*) "Scheme")
 (kl:set (quote *implementation*) "chez-scheme")
-(kl:set (quote *port*) "0.18")
+(kl:set (quote *port*) "0.19")
 (kl:set (quote *release*) (call-with-values scheme-version-number (lambda (major minor patch) (format "~s.~s.~s" major minor patch))))
 (kl:set (quote *porters*) "Bruno Deferrari")
 (kl:set (quote *sterror*) (standard-error-port))
